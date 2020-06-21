58b97846497d19033cbe08845a70fcb772bc57b4
Heido Nomm <heidonomm@gmail.com>
2020-06-21 17:09:08 +0300

    updated file name

--- train.py
+++ train.py
@@ -94,7 +94,7 @@ def train(data_path):
     if agent.train_data_size == -1:
         game_queue_size = agent.batch_size * 5
         game_queue = []
-    f = open("bert_accuracies_avg.txt", "a+")
+    f = open("bert_accuracies_avg_1000.txt", "a+")
     episode_no = 0
     if agent.train_data_size == -1:
         # endless mode
